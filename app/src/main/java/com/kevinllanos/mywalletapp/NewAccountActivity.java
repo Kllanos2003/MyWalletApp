2650f718c70a7be734013ed266b1353a4ec081ec
B13 <kllanos@unab.edu.co>
2023-09-06 09:51:46 -0500

    Botones (8/09/2023)

--- app/src/main/java/com/kevinllanos/mywalletapp/NewAccountActivity.java
+++ app/src/main/java/com/kevinllanos/mywalletapp/NewAccountActivity.java
@@ -2,13 +2,32 @@ package com.kevinllanos.mywalletapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.QuickContactBadge;
 
 public class NewAccountActivity extends AppCompatActivity {
+
+    private ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        ivBack = findViewById(R.id.iv_back);
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(NewAccountActivity.this , MainActivity.class);
+                startActivities(myIntent);
+
+            }
+        });
     }
+
+
 }
