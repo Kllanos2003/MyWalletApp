e6701c54e1e6553f50c757b90a9ea234786d3055
B13 <kllanos@unab.edu.co>
2023-09-08 09:47:27 -0500

    Account.java (8/09/2023)

--- app/src/main/java/com/kevinllanos/mywalletapp/ListAccountActivity.java
+++ app/src/main/java/com/kevinllanos/mywalletapp/ListAccountActivity.java
@@ -14,10 +14,16 @@ public class ListAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_account);
+        loadFakeData();
     }
 
     private void loadFakeData(){
         Account myAccount1 = new Account("Bancolombia" , "Cuenta Corriente", 1000012.0);
+        Account myAccount2 = new Account("Davivienda" , "Cuenta Ahorros", 5316532.0);
+        Account myAccount3 = new Account("Bancolombia" , "Tarjeta Creadito", 78465465.0);
+        listAccountRv.add(myAccount1);
+        listAccountRv.add(myAccount2);
+        listAccountRv.add(myAccount3);
     }
 
 }
